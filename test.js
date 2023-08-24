d7359e2163547acec69c49a6ee722172adb5b3a6
giaunguyen2604 <giau.nguyen@executionlab.asia>
2023-08-24 13:02:06 +0700

    vim

--- test.js
+++
@@ -1,5 +0,0 @@
-function removeZeroToLeft(){
-    const input = [1, -3, 2, 0, 5, 0, 10, 0, -1]
-    const filter = input.filter(i => i !== 0)
-    return [...Array(input.length - filter.length).fill(0),...filter]
-}
